e2eb8b36517d5204188b7c4da7e5bcefb5a58920
Emmanuel Oyiboke <emmanueloyiboke@gmail.com>
2023-03-17 12:34:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LinkTree from "./components/links/linktree";
+import Footer from "./components/footer/footer";
+
+function App() {
+  return (
+    <div className='lg:px-[100px] md:PX-[80PX] xsm:px-[60px] px-[10px] py-10'>
+      <LinkTree />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
